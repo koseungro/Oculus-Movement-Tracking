d41c4ed75eb935e1d4c36c0da55ca51f26232220
koseungro <goseungro@naver.com>
2024-05-17 17:03:04 +0900

    샘플 신 수정

--- Assets/Scripts/Core/Logger.cs
+++ Assets/Scripts/Core/Logger.cs
@@ -35,8 +35,19 @@ public class Logger : Singleton<Logger>
     /// Y 축 기준 눈의 회전 값
     /// </summary>
     private float eyeRot_Y = 0;
+    /// <summary>
+    /// Z 축 기준 눈의 회전 값
+    /// </summary>
+    private float eyeRot_Z = 0;
 
     [SerializeField] private GameObject controller;
+    private float controllerPos_X;
+    private float controllerPos_Y;
+    private float controllerPos_Z;
+
+    private float controllerRot_X;
+    private float controllerRot_Y;
+    private float controllerRot_Z;
 
     void Awake()
     {
@@ -69,12 +80,39 @@ public class Logger : Singleton<Logger>
         // 눈 회전 값 추출
         eyeRot_X = MathF.Floor((targetEye.transform.localEulerAngles.x < 180f ? targetEye.transform.localEulerAngles.x : targetEye.transform.localEulerAngles.x - 360f) * 1000) * 0.001f;
         eyeRot_Y = MathF.Floor((targetEye.transform.localEulerAngles.y < 180f ? targetEye.transform.localEulerAngles.y : targetEye.transform.localEulerAngles.y - 360f) * 1000) * 0.001f;
+        eyeRot_Z = MathF.Floor((targetEye.transform.localEulerAngles.z < 180f ? targetEye.transform.localEulerAngles.z : targetEye.transform.localEulerAngles.z - 360f) * 1000) * 0.001f;
 
         eyeText.Clear();
-        eyeText.AppendLine($"<size=0.12><<b>{targetEye.name}</b>></size>  rotation value(axis)\n");
-        eyeText.AppendLine($"<color=green>X : {eyeRot_X}  Y : {eyeRot_Y}</color>");
+        eyeText.AppendLine($"<size=0.12><<b>{targetEye.name}</b>></size> Rotation value(axis)\n");
+        eyeText.AppendLine($"<color=green>X : {eyeRot_X}</color>");
+        eyeText.AppendLine($"<color=green>Y : {eyeRot_Y}</color>");
+        eyeText.AppendLine($"<color=green>Z : {eyeRot_Z}</color>");
 
-        debugAreaText.text = eyeText.ToString();
+        // 콘트롤러 값 추출
+        controllerPos_X = MathF.Floor((controller.transform.position.x * 1000f)) * 0.001f;
+        controllerPos_Y = MathF.Floor((controller.transform.position.y * 1000f)) * 0.001f;
+        controllerPos_Z = MathF.Floor((controller.transform.position.z * 1000f)) * 0.001f;
+
+        controllerRot_X = MathF.Floor((controller.transform.localEulerAngles.x < 180f ? controller.transform.localEulerAngles.x : controller.transform.localEulerAngles.x - 360f) * 1000) * 0.001f;
+        controllerRot_Y = MathF.Floor((controller.transform.localEulerAngles.y < 180f ? controller.transform.localEulerAngles.y : controller.transform.localEulerAngles.y - 360f) * 1000) * 0.001f;
+        controllerRot_Z = MathF.Floor((controller.transform.localEulerAngles.z < 180f ? controller.transform.localEulerAngles.z : controller.transform.localEulerAngles.z - 360f) * 1000) * 0.001f;
+
+
+        controllerText.Clear();
+        controllerText.AppendLine();
+        controllerText.AppendLine($"<size=0.12><<b>{controller.name}</b>></size>\n");
+        controllerText.AppendLine($"<size=0.11>Position</size>");
+        controllerText.AppendLine($"<color=green>X : {controllerPos_X}</color>");
+        controllerText.AppendLine($"<color=green>Y : {controllerPos_Y}</color>");
+        controllerText.AppendLine($"<color=green>Z : {controllerPos_Z}</color>");
+
+        controllerText.AppendLine($"<size=0.11>Rotation</size>");
+        controllerText.AppendLine($"<color=green>X : {controllerRot_X}</color>");
+        controllerText.AppendLine($"<color=green>Y : {controllerRot_Y}</color>");
+        controllerText.AppendLine($"<color=green>Z : {controllerRot_Z}</color>");
+
+
+        debugAreaText.text = eyeText.ToString() + controllerText.ToString();
     }
     public void Clear() => debugAreaText.text = string.Empty;
 
